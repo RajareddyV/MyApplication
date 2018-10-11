dcb42a7f666a85e8d61362aacc5020406276b9ae
RajareddyV <mobandroitest4@gmail.com>
2018-10-11 22:43:59 +0530

    second commit

--- app/src/main/java/com/example/rajareddy/hello/MainActivity.java
+++ app/src/main/java/com/example/rajareddy/hello/MainActivity.java
@@ -20,7 +20,7 @@ public static final String TAG=MainActivity.class.getSimpleName();
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplicationContext(),Second.class));
-finish();
+            finish();
             }
         });
 
